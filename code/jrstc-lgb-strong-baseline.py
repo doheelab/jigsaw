0650e76a707b1a34734d2f5ec6c82109091577a9
doheelab <dohee0203z@skku.edu>
2021-12-29 22:24:22 +0900

    :recycle: refactor codes & add eval_weights

--- code/jrstc-lgb-strong-baseline.py
+++ code/jrstc-lgb-strong-baseline.py
@@ -177,16 +177,14 @@ print(df_val["upper_2"].mean(), df_val["upper_2"].std())
 df_val["upper_1"].hist(bins=100)
 df_val["upper_2"].hist(bins=100)
 
-# ## Train pipeline
-#
-# - Load folds data
-# - train pipeline
-# - Predict on validation data
-# - Predict on test data
+# # ## Train pipeline
+# #
+# # - Load folds data
+# # - train pipeline
+# # - Predict on validation data
+# # - Predict on test data
 
-# ### Toxic data
-
-
+# # ### Toxic data
 def train_on_raw_df():
     val_preds_arr1 = np.zeros((df_val.shape[0], n_folds))
     val_preds_arr2 = np.zeros((df_val.shape[0], n_folds))
@@ -272,8 +270,6 @@ def train_on_clean_data():
 
 
 # ## Ruddit data pipeline
-
-
 def train_on_ruddit_data():
 
     val_preds_arr1_ = np.zeros((df_val.shape[0], n_folds))
@@ -321,24 +317,7 @@ val_preds_arr1, val_preds_arr2, test_preds_arr = train_on_raw_df()
 val_preds_arr1c, val_preds_arr2c, test_preds_arrc = train_on_clean_data()
 val_preds_arr1_, val_preds_arr2_, test_preds_arr_ = train_on_ruddit_data()
 
-
-print(" Toxic CLEAN data ")
-p5 = val_preds_arr1c.mean(axis=1)
-p6 = val_preds_arr2c.mean(axis=1)
-
-print(f"Validation Accuracy is { np.round((p5 < p6).mean() * 100,2)}")
-
-# %%
-print(" Toxic CLEAN data new ")
-p5 = val_preds_arr1c.mean(axis=1)
-p6 = val_preds_arr2c.mean(axis=1)
-
-print(f"Validation Accuracy is { np.round((p5 < p6).mean() * 100,2)}")
-
-# %% [markdown]
 # # Validate the pipeline
-
-# %%
 print(" Toxic data ")
 p1 = val_preds_arr1.mean(axis=1)
 p2 = val_preds_arr2.mean(axis=1)
@@ -369,28 +348,19 @@ def find_weights():
             p2_wt = w1 * p2 + w2 * p4 + w3 * p6
             wts_acc.append((w1, w2, w3, np.round((p1_wt < p2_wt).mean() * 100, 2)))
     sorted(wts_acc, key=lambda x: x[3], reverse=True)[:5]
-
     w1, w2, w3, _ = sorted(wts_acc, key=lambda x: x[2], reverse=True)[0]
+    return w1, w2, w3
+
+
+w1, w2, w3 = find_weights()
+
+
+def eval_weights(w1, w2, w3):
     p1_wt = w1 * p1 + w2 * p3 + w3 * p5
     p2_wt = w1 * p2 + w2 * p4 + w3 * p6
-    return w1, w2, w3, p1_wt, p2_wt
+    score = np.round((p1_wt < p2_wt).mean() * 100, 2)
+    return score
 
-
-w1, w2, w3, p1_wt, p2_wt = find_weights()
-
-# ## Analyze bad predictions
-# ### Incorrect predictions with similar scores
-# ### Incorrect predictions with different scores
-
-df_val["p1"] = p1_wt
-df_val["p2"] = p2_wt
-df_val["diff"] = np.abs(p2_wt - p1_wt)
-df_val["correct"] = (p1_wt < p2_wt).astype("int")
-
-
-df_val[df_val.correct == 0].sort_values("diff", ascending=True).head(20)
-#### Some of these just look incorrectly tagged
-df_val[df_val.correct == 0].sort_values("diff", ascending=False).head(20)
 
 # # Predict on test data
 df_sub["score"] = (
@@ -398,169 +368,158 @@ df_sub["score"] = (
     + w2 * test_preds_arr_.mean(axis=1)
     + w3 * test_preds_arrc.mean(axis=1)
 )
-
-# ## Correct the rank ordering
-df_sub["score"].count() - df_sub["score"].nunique()
-same_score = df_sub["score"].value_counts().reset_index()[:10]
-same_score
-
-df_sub[df_sub["score"].isin(same_score["index"].tolist())]
 df_sub[["comment_id", "score"]].to_csv("submission.csv", index=False)
 
 
 ############################################
 
-# # Bert Ensemble
-import os
-import gc
-import cv2
-import copy
-import time
-import torch
-import random
-import numpy as np
-import pandas as pd
-from tqdm import tqdm
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoTokenizer, AutoModel
+# # # Bert Ensemble
+# import os
+# import gc
+# import cv2
+# import copy
+# import time
+# import torch
+# import random
+# import numpy as np
+# import pandas as pd
+# from tqdm import tqdm
+# import torch.nn as nn
+# from torch.utils.data import Dataset, DataLoader
+# from transformers import AutoTokenizer, AutoModel
 
-os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
-CONFIG = dict(
-    seed=42,
-    model_name="../input/roberta-base",
-    test_batch_size=64,
-    max_length=128,
-    num_classes=1,
-    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
-)
+# CONFIG = dict(
+#     seed=42,
+#     model_name="../input/roberta-base",
+#     test_batch_size=64,
+#     max_length=128,
+#     num_classes=1,
+#     device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+# )
 
-CONFIG["tokenizer"] = AutoTokenizer.from_pretrained(CONFIG["model_name"])
+# CONFIG["tokenizer"] = AutoTokenizer.from_pretrained(CONFIG["model_name"])
 
-MODEL_PATHS = [
-    "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-0.bin",
-    "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-1.bin",
-    "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-2.bin",
-    "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-3.bin",
-    "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-4.bin",
-]
-
-
-def set_seed(seed=42):
-    """Sets the seed of the entire notebook so results are the same every time we run.
-    This is for REPRODUCIBILITY."""
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    os.environ["PYTHONHASHSEED"] = str(seed)
+# MODEL_PATHS = [
+#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-0.bin",
+#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-1.bin",
+#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-2.bin",
+#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-3.bin",
+#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-4.bin",
+# ]
 
 
-class JigsawDataset(Dataset):
-    def __init__(self, df, tokenizer, max_length):
-        self.df = df
-        self.max_len = max_length
-        self.tokenizer = tokenizer
-        self.text = df["text"].values
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, index):
-        text = self.text[index]
-        inputs = self.tokenizer.encode_plus(
-            text,
-            truncation=True,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            padding="max_length",
-        )
-
-        ids = inputs["input_ids"]
-        mask = inputs["attention_mask"]
-
-        return {
-            "ids": torch.tensor(ids, dtype=torch.long),
-            "mask": torch.tensor(mask, dtype=torch.long),
-        }
+# def set_seed(seed=42):
+#     """Sets the seed of the entire notebook so results are the same every time we run.
+#     This is for REPRODUCIBILITY."""
+#     np.random.seed(seed)
+#     random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
+#     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
-class JigsawModel(nn.Module):
-    def __init__(self, model_name):
-        super(JigsawModel, self).__init__()
-        self.model = AutoModel.from_pretrained(model_name)
-        self.drop = nn.Dropout(p=0.2)
-        self.fc = nn.Linear(768, CONFIG["num_classes"])
+# class JigsawDataset(Dataset):
+#     def __init__(self, df, tokenizer, max_length):
+#         self.df = df
+#         self.max_len = max_length
+#         self.tokenizer = tokenizer
+#         self.text = df["text"].values
 
-    def forward(self, ids, mask):
-        out = self.model(input_ids=ids, attention_mask=mask, output_hidden_states=False)
-        out = self.drop(out[1])
-        outputs = self.fc(out)
-        return outputs
+#     def __len__(self):
+#         return len(self.df)
 
+#     def __getitem__(self, index):
+#         text = self.text[index]
+#         inputs = self.tokenizer.encode_plus(
+#             text,
+#             truncation=True,
+#             add_special_tokens=True,
+#             max_length=self.max_len,
+#             padding="max_length",
+#         )
 
-@torch.no_grad()
-def valid_fn(model, dataloader, device):
-    model.eval()
+#         ids = inputs["input_ids"]
+#         mask = inputs["attention_mask"]
 
-    dataset_size = 0
-    running_loss = 0.0
-
-    PREDS = []
-
-    bar = tqdm(enumerate(dataloader), total=len(dataloader))
-    for step, data in bar:
-        ids = data["ids"].to(device, dtype=torch.long)
-        mask = data["mask"].to(device, dtype=torch.long)
-
-        outputs = model(ids, mask)
-        PREDS.append(outputs.view(-1).cpu().detach().numpy())
-
-    PREDS = np.concatenate(PREDS)
-    gc.collect()
-
-    return PREDS
+#         return {
+#             "ids": torch.tensor(ids, dtype=torch.long),
+#             "mask": torch.tensor(mask, dtype=torch.long),
+#         }
 
 
-def inference(model_paths, dataloader, device):
-    final_preds = []
-    for i, path in enumerate(model_paths):
-        model = JigsawModel(CONFIG["model_name"])
-        model.to(CONFIG["device"])
-        model.load_state_dict(torch.load(path))
+# class JigsawModel(nn.Module):
+#     def __init__(self, model_name):
+#         super(JigsawModel, self).__init__()
+#         self.model = AutoModel.from_pretrained(model_name)
+#         self.drop = nn.Dropout(p=0.2)
+#         self.fc = nn.Linear(768, CONFIG["num_classes"])
 
-        print(f"Getting predictions for model {i+1}")
-        preds = valid_fn(model, dataloader, device)
-        final_preds.append(preds)
-
-    final_preds = np.array(final_preds)
-    final_preds = np.mean(final_preds, axis=0)
-    return final_preds
+#     def forward(self, ids, mask):
+#         out = self.model(input_ids=ids, attention_mask=mask, output_hidden_states=False)
+#         out = self.drop(out[1])
+#         outputs = self.fc(out)
+#         return outputs
 
 
-set_seed(CONFIG["seed"])
-df = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
-df.head()
+# @torch.no_grad()
+# def valid_fn(model, dataloader, device):
+#     model.eval()
 
-test_dataset = JigsawDataset(df, CONFIG["tokenizer"], max_length=CONFIG["max_length"])
-test_loader = DataLoader(
-    test_dataset,
-    batch_size=CONFIG["test_batch_size"],
-    num_workers=2,
-    shuffle=False,
-    pin_memory=True,
-)
-preds1 = inference(MODEL_PATHS, test_loader, CONFIG["device"])
+#     dataset_size = 0
+#     running_loss = 0.0
 
-# %%
-preds = (preds1 - preds1.min()) / (preds1.max() - preds1.min())
+#     PREDS = []
 
-# %%
-df_sub["score"] = df_sub["score"] * 0.85 + preds * 0.15
+#     bar = tqdm(enumerate(dataloader), total=len(dataloader))
+#     for step, data in bar:
+#         ids = data["ids"].to(device, dtype=torch.long)
+#         mask = data["mask"].to(device, dtype=torch.long)
 
-# %%
-df_sub[["comment_id", "score"]].to_csv("submission.csv", index=False)
+#         outputs = model(ids, mask)
+#         PREDS.append(outputs.view(-1).cpu().detach().numpy())
+
+#     PREDS = np.concatenate(PREDS)
+#     gc.collect()
+
+#     return PREDS
+
+
+# def inference(model_paths, dataloader, device):
+#     final_preds = []
+#     for i, path in enumerate(model_paths):
+#         model = JigsawModel(CONFIG["model_name"])
+#         model.to(CONFIG["device"])
+#         model.load_state_dict(torch.load(path))
+
+#         print(f"Getting predictions for model {i+1}")
+#         preds = valid_fn(model, dataloader, device)
+#         final_preds.append(preds)
+
+#     final_preds = np.array(final_preds)
+#     final_preds = np.mean(final_preds, axis=0)
+#     return final_preds
+
+
+# set_seed(CONFIG["seed"])
+# df = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
+# df.head()
+
+# test_dataset = JigsawDataset(df, CONFIG["tokenizer"], max_length=CONFIG["max_length"])
+# test_loader = DataLoader(
+#     test_dataset,
+#     batch_size=CONFIG["test_batch_size"],
+#     num_workers=2,
+#     shuffle=False,
+#     pin_memory=True,
+# )
+# preds1 = inference(MODEL_PATHS, test_loader, CONFIG["device"])
+
+# preds = (preds1 - preds1.min()) / (preds1.max() - preds1.min())
+# df_sub = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
+# df_sub["score"] = df_sub["score"] * 0.85 + preds * 0.15
+# df_sub[["comment_id", "score"]].to_csv("submission.csv", index=False)
 
