252440520d7acd4f375e140d9803f7b802a64b24
doheelab <dohee0203z@skku.edu>
2022-01-01 01:56:23 +0000

    :zap: add_df_sub

--- code/ridge_853.py
+++ code/ridge_853.py
@@ -1,4 +1,4 @@
-# %%
+
 import gc
 import re
 import scipy
@@ -14,6 +14,14 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.linear_model import Ridge, LinearRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 import warnings
+import copy
+
+# to change
+
+# save_dir = "/kaggle/working"
+save_dir = "../save"
+
+
 
 warnings.filterwarnings("ignore")
 
@@ -23,6 +31,11 @@ pd.options.display.max_colwidth = 300
 # # Training data
 # ## Convert the label to SUM of all toxic labels (This might help with maintaining toxicity order of comments)
 df = pd.read_csv("../input/jigsaw-toxic-comment-classification-challenge/train.csv")
+
+# ## Load Validation and Test data
+df_val = pd.read_csv("../input/jigsaw-toxic-severity-rating/validation_data.csv")
+df_sub = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
+
 df["severe_toxic"] = df.severe_toxic * 2
 df["y"] = (
     df[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].sum(
@@ -37,28 +50,9 @@ df["y"].value_counts()
 
 # ## Create 3 versions of the data
 n_folds = 7
-# save_dir = "/kaggle/working"
-save_dir = "../save"
 ruddit_path = "../input/ruddit-jigsaw-dataset/Dataset"
 
 
-
-def save_raw_df(df):
-    frac_1 = 0.3
-    frac_1_factor = 1.2
-    for fld in range(n_folds):
-        print(f"Fold: {fld}")
-        tmp1 = df[df.y > 0].sample(frac=frac_1, random_state=10 * (fld + 1))
-        tmp2 = df[df.y == 0].sample(
-            n=int(len(df[df.y > 0]) * frac_1 * frac_1_factor),
-            random_state=10 * (fld + 1),
-        )
-        tmp_df = pd.concat([tmp1, tmp2], axis=0).sample(
-            frac=1, random_state=10 * (fld + 1)
-        )
-        tmp_df.to_csv(f"{save_dir}/df_fld{fld}.csv", index=False)
-        print(tmp_df.shape)
-        print(tmp_df["y"].value_counts())
 
 
 # # Create 3 versions of __clean__ data
@@ -95,12 +89,43 @@ def clean(data, col):
     )
     return data
 
+def add_df_sub(df):
+    df_sub_to_add = copy.copy(df_sub)
+    df_sub_to_add['y'] = np.nan
+    df_sub_to_add = df_sub_to_add[['text', 'y']]
+    return pd.concat([df, df_sub_to_add], axis=0)
+
+
+
+def save_raw_df(df):
+    frac_1 = 0.3
+    frac_1_factor = 1.2
+
+    df = add_df_sub(df)
+
+    for fld in range(n_folds):
+        print(f"Fold: {fld}")
+        tmp1 = df[df.y > 0].sample(frac=frac_1, random_state=10 * (fld + 1))
+        tmp2 = df[df.y == 0].sample(
+            n=int(len(df[df.y > 0]) * frac_1 * frac_1_factor),
+            random_state=10 * (fld + 1),
+        )
+        tmp_df = pd.concat([tmp1, tmp2], axis=0).sample(
+            frac=1, random_state=10 * (fld + 1)
+        )
+        tmp_df.to_csv(f"{save_dir}/df_fld{fld}.csv", index=False)
+        print(tmp_df.shape)
+        print(tmp_df["y"].value_counts())
+
 
 def save_cleaned_df(df):
     df = clean(df, "text")
     n_folds = 7
     frac_1 = 0.3
     frac_1_factor = 1.2
+
+    df = add_df_sub(df)
+
     for fld in range(n_folds):
         tmp_df = pd.concat(
             [
@@ -117,8 +142,11 @@ def save_cleaned_df(df):
         print(tmp_df["y"].value_counts())
 
 
-def save_ruddit_df():
 
+
+
+
+def save_ruddit_df():
     # ## Ruddit data
     df_ = pd.read_csv(f"{ruddit_path}/ruddit_with_text.csv")
     print(df_.shape)
@@ -127,6 +155,8 @@ def save_ruddit_df():
     )
     df_["y"] = (df_["y"] - df_.y.min()) / (df_.y.max() - df_.y.min())
 #     df_.y.hist()
+
+    df_ = add_df_sub(df_)
 
     # # Create 3 versions of data
     n_folds = 7
@@ -143,9 +173,7 @@ save_cleaned_df(df)
 save_raw_df(df)
 save_ruddit_df()
 
-# ## Load Validation and Test data
-df_val = pd.read_csv("../input/jigsaw-toxic-severity-rating/validation_data.csv")
-df_sub = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
+
 
 # Create Sklearn Pipeline with
 # TFIDF - Take 'char_wb' as analyzer to capture subwords well
@@ -203,6 +231,7 @@ def train_on_raw_df():
             f" ****************************** FOLD: {fld} ******************************"
         )
         df = pd.read_csv(f"{save_dir}/df_fld{fld}.csv")
+        df.dropna(inplace=True)
         print(df.shape)
 
         features = FeatureUnion(
@@ -246,6 +275,7 @@ def train_on_clean_data():
             f" ****************************** FOLD: {fld} ******************************"
         )
         df = pd.read_csv(f"{save_dir}/df_clean_fld{fld}.csv")
+        df.dropna(inplace=True)
         print(df.shape)
 
         features = FeatureUnion(
@@ -291,6 +321,7 @@ def train_on_ruddit_data():
             f" ****************************** FOLD: {fld} ******************************"
         )
         df = pd.read_csv(f"{save_dir}/df2_fld{fld}.csv")
+        df.dropna(inplace=True)
         print(df.shape)
 
         features = FeatureUnion(
@@ -411,159 +442,4 @@ same_score
 
 df_sub[df_sub["score"].isin(same_score["index"].tolist())]
 df_sub[["comment_id", "score"]].to_csv("submission.csv", index=False)
-
-
-# %%
-
-# ############## BERT
-
-# # # Bert Ensemble
-# import os
-# import gc
-# import cv2
-# import copy
-# import time
-# import torch
-# import random
-# import numpy as np
-# import pandas as pd
-# from tqdm import tqdm
-# import torch.nn as nn
-# from torch.utils.data import Dataset, DataLoader
-# from transformers import AutoTokenizer, AutoModel
-
-# os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
-
-# CONFIG = dict(
-#     seed=42,
-#     model_name="../input/roberta-base",
-#     test_batch_size=64,
-#     max_length=128,
-#     num_classes=1,
-#     device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
-# )
-
-# CONFIG["tokenizer"] = AutoTokenizer.from_pretrained(CONFIG["model_name"])
-
-# MODEL_PATHS = [
-#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-0.bin",
-#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-1.bin",
-#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-2.bin",
-#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-3.bin",
-#     "../input/pytorch-w-b-jigsaw-starter/Loss-Fold-4.bin",
-# ]
-
-
-# def set_seed(seed=42):
-#     """Sets the seed of the entire notebook so results are the same every time we run.
-#     This is for REPRODUCIBILITY."""
-#     np.random.seed(seed)
-#     random.seed(seed)
-#     torch.manual_seed(seed)
-#     torch.cuda.manual_seed(seed)
-#     torch.backends.cudnn.deterministic = True
-#     torch.backends.cudnn.benchmark = False
-#     os.environ["PYTHONHASHSEED"] = str(seed)
-
-
-# class JigsawDataset(Dataset):
-#     def __init__(self, df, tokenizer, max_length):
-#         self.df = df
-#         self.max_len = max_length
-#         self.tokenizer = tokenizer
-#         self.text = df["text"].values
-
-#     def __len__(self):
-#         return len(self.df)
-
-#     def __getitem__(self, index):
-#         text = self.text[index]
-#         inputs = self.tokenizer.encode_plus(
-#             text,
-#             truncation=True,
-#             add_special_tokens=True,
-#             max_length=self.max_len,
-#             padding="max_length",
-#         )
-
-#         ids = inputs["input_ids"]
-#         mask = inputs["attention_mask"]
-
-#         return {
-#             "ids": torch.tensor(ids, dtype=torch.long),
-#             "mask": torch.tensor(mask, dtype=torch.long),
-#         }
-
-
-# class JigsawModel(nn.Module):
-#     def __init__(self, model_name):
-#         super(JigsawModel, self).__init__()
-#         self.model = AutoModel.from_pretrained(model_name)
-#         self.drop = nn.Dropout(p=0.2)
-#         self.fc = nn.Linear(768, CONFIG["num_classes"])
-
-#     def forward(self, ids, mask):
-#         out = self.model(input_ids=ids, attention_mask=mask, output_hidden_states=False)
-#         out = self.drop(out[1])
-#         outputs = self.fc(out)
-#         return outputs
-
-
-# @torch.no_grad()
-# def valid_fn(model, dataloader, device):
-#     model.eval()
-
-#     dataset_size = 0
-#     running_loss = 0.0
-
-#     PREDS = []
-
-#     bar = tqdm(enumerate(dataloader), total=len(dataloader))
-#     for step, data in bar:
-#         ids = data["ids"].to(device, dtype=torch.long)
-#         mask = data["mask"].to(device, dtype=torch.long)
-
-#         outputs = model(ids, mask)
-#         PREDS.append(outputs.view(-1).cpu().detach().numpy())
-
-#     PREDS = np.concatenate(PREDS)
-#     gc.collect()
-
-#     return PREDS
-
-
-# def inference(model_paths, dataloader, device):
-#     final_preds = []
-#     for i, path in enumerate(model_paths):
-#         model = JigsawModel(CONFIG["model_name"])
-#         model.to(CONFIG["device"])
-#         model.load_state_dict(torch.load(path))
-
-#         print(f"Getting predictions for model {i+1}")
-#         preds = valid_fn(model, dataloader, device)
-#         final_preds.append(preds)
-
-#     final_preds = np.array(final_preds)
-#     final_preds = np.mean(final_preds, axis=0)
-#     return final_preds
-
-
-# set_seed(CONFIG["seed"])
-# df = pd.read_csv("../input/jigsaw-toxic-severity-rating/comments_to_score.csv")
-# df.head()
-
-# test_dataset = JigsawDataset(df, CONFIG["tokenizer"], max_length=CONFIG["max_length"])
-# test_loader = DataLoader(
-#     test_dataset,
-#     batch_size=CONFIG["test_batch_size"],
-#     num_workers=2,
-#     shuffle=False,
-#     pin_memory=True,
-# )
-# preds1 = inference(MODEL_PATHS, test_loader, CONFIG["device"])
-
-# preds = (preds1 - preds1.min()) / (preds1.max() - preds1.min())
-# df_sub["score"] = df_sub["score"] * 0.85 + preds * 0.15
-# df_sub[["comment_id", "score"]].to_csv("submission.csv", index=False)
-
 
